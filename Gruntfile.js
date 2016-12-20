956113a516a1eca53ec9b480f388fdc3153612c4
Bryce Summers <summoose@gmail.com>
2016-12-20 11:26:57 -0500

    Updated this historical branch to a concat based build system.

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,12 +9,38 @@ module.exports = function (grunt) {
           'index.html': 'index.tpl.html'
         }
       }
-    }
+    },
+    uglify: {
+        bar: {
+            // uglify task "bar" target options and files go here.
+        }
+    },
+
+    concat: {
+        options: {
+            separator: '\n',
+            banner: '/*! Sim Urban, a project by Bryce Summers.\n *  Single File concatenated by Grunt Concatenate on <%= grunt.template.today("dd-mm-yyyy") %>\n */\n'
+        },
+        dist: {
+            // Include one level down, two levels down, three levels down, then main
+            src: ['src/namespace.js', 'lib/*/*.js', 'lib/*/*/*.js', 'lib/*/*/*/*.js', 'src/main.js'],
+            dest: 'builds/a_current_build.js',
+        },
+    },
+
   });
-  
+ 
+  // Source Inclusion.
   grunt.loadNpmTasks('grunt-include-source');
+
+  // Minification and Uglification.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // File concatenation into one file.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
   grunt.registerTask('default', function (target) {
-    grunt.task.run('includeSource');
+    //grunt.task.run('includeSource');
+    grunt.task.run('concat');
   });
 };
